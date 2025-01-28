91a20b45f9bd9ca906b518d53add2e0583704cd6
abhishekch33 <abhishek.2101134ec@iiitbh.ac.in>
2025-01-28 17:27:41 +0530

    Friends

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './src',
-  build: {
-    outDir: '../dist',
-  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
-
--- server/index.js
+++ server/index.js
@@ -5,10 +5,13 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import friendRoutes from './routes/friends.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 // Middleware
 app.use(cors());
@@ -23,6 +26,17 @@ mongoose.connect(process.env.MONGODB_URI)
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/friends', friendRoutes);
+
+// http://localhost:5000 => backend,frontend
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	// react app
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
